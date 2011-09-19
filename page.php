746c9c3db9bb41d3ba05de5447d97dec78f7749a
Lafon Sylvain <sylaf@hotmail.fr>
2011-09-19 10:54:55 +0200

    Desactivation du cache

--- page.php
+++ page.php
@@ -73,6 +73,7 @@
 			<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
 				<head>
 					<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+					<meta http-equiv="pragma"		content="no-cache"				   />
 					<link rel="stylesheet" media="screen" href="style.css" type="text/css"/>
 					<title>Site - Chez Bob</title>
 				</head>
