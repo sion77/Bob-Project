0d759d309a36b6b20fe55c04afe9348f4e43fa41
Francois Maingret <francois.maingret@u-psud.fr>
2011-10-03 08:18:43 +0200

    Synchro

--- php/page.php
+++ php/page.php
@@ -18,8 +18,8 @@
 				afficheCategories();
 			break;
 			
-			case "CATEGORIES-LOCATION":
-				afficheCategoriesLocation();
+			case "SOUSCATEGORIES":
+				afficheSousCategories();
 			break;
 			
 			case "ABOUT":
@@ -67,9 +67,11 @@
 			<ul id="menu">
 				<li><a href='index.php?page=CATEGORIES'>Acheter</a>
 					<ul class="sous-menu">
-						<span class="first-elem-sous-menu"><li><a href='#'>Porn Pics</a></li></span>
-						<li><a href='#'>Porn Pics</a></li>
-						<span class="last-elem-sous-menu"><li><a href='#'>Porn Pics</a></li></span>
+						<span class="first-elem-sous-menu"><li><a href='index.php?page=SOUSCATEGORIES'>Jardin</a></li></span>
+						<li><a href='#'>Menuiserie</a></li>
+						<li><a href='#'>Sols</a></li>
+						<li><a href='#'>Quilcaillerie</a></li>
+						<span class="last-elem-sous-menu"><li><a href='#'>Peinture</a></li></span>
 					</ul>
 				</li>
 				<li><a href='index.php?page=CATEGORIES-LOCATION'>Louer</a>
