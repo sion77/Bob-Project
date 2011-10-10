cdd6e4832fa1eaeb0e0ffaa0035abc7979ea8f38
Francois Maingret <francois.maingret@u-psud.fr>
2011-10-10 10:28:11 +0200

    Design du pannel admin

--- php/page.php
+++ php/page.php
@@ -66,7 +66,7 @@
 		{
 			?>
 				
-				<span class="etat_non_connecte">
+				<span class="etat-co-espace-membre">
 					Vous n'êtes pas connecté, veuillez vous <a href="index.php?page=CONNECTION">Connecter</a>
 					ou vous <a href="index.php?page=INSCRIPTION">Inscrire !</a>
 				</span>
@@ -77,14 +77,16 @@
 		// Connecté
 		else
 		{
-			echo '<span class="etat_connecte">';
-				echo $_SESSION["pseudo"];
-				if($_SESSION["admin"] == true)
-				{
-					echo '<a href="index.php?admin=ACCUEIL">Administrer</a>';
-				}
-				echo '<a href="index.php?action=DECONNECTION">Deconnection</a>';
-			echo '</span>';
+			echo $_SESSION["pseudo"];
+			if($_SESSION["admin"] == true)
+			{
+				?>
+					<a href="index.php?admin=ACCUEIL">Administrer</a>
+				<?php
+			}
+			?>
+				<a href="index.php?action=DECONNECTION">Deconnection</a>
+			<?php
 		}
 	}
 	
