19124a20fbb4c6c2840b6d80941fe817881a41cf
sylafrs <sylaf@hotmail.fr>
2011-11-15 23:26:50 +0100

    merge

--- script.js
+++ script.js
@@ -10,39 +10,21 @@ function checkPseudo()
 	
 	if(verifAlphaNum(pseudo) == true)
 	{
-<<<<<<< HEAD
-		document.getElementById("pseudoEtat").innerHTML = "Pseudo correct (syntaxe)";
-=======
 		xhr.open('GET', 'http://localhost/Bob-Project/index.php?ajax=existe_membre&pseudo=' + pseudo);
 		xhr.send(null);
 		xhr.onreadystatechange = function() {
 			if(xhr.readyState == 4 && xhr.status == 200) 
 			{
 				alert(xhr.responseText);
-			}
-		// Ici
-		};
->>>>>>> 11612da49e037e12b893bf180e3ee20f93e3f37a
+			}		
+		}; // Ici
 	}
 	else
 	{
 		document.getElementById("pseudoEtat").innerHTML = "Pseudo incorrect (syntaxe)";
-	}
-	
-	
-<<<<<<< HEAD
-	xhr.open('GET', 'http://localhost/Bob-Project/index.php?ajax=existe_membre&pseudo=' + pseudo);
-	xhr.send(null);
-	xhr.onreadystatechange = function() {
-    if(xhr.readyState == 4 && xhr.status == 200) 
-	{
-		alert(xhr.responseText);
-    }
-};
-=======
-//};
->>>>>>> 11612da49e037e12b893bf180e3ee20f93e3f37a
-	
+	}	
+
+//};	
 	
 }
 
