a392854c1953c71028efd12eb5bd6c767a3840a4
sylafrs <sylaf@hotmail.fr>
2011-11-15 23:39:30 +0100

    voilou

--- script.js
+++ script.js
@@ -8,31 +8,48 @@ function checkPseudo()
 {
 	var pseudo = encodeURIComponent(document.getElementById("pseudo").value);
 	
-	if(verifAlphaNum(pseudo) == true)
-	{	
-		var xhr = new XMLHttpRequest();
-		xhr.open('GET', 'http://localhost/Bob-Project/index.php?ajax=existe_membre&pseudo=' + pseudo);
-		xhr.send(null);
-		xhr.onreadystatechange = function() {
-			if(xhr.readyState == 4 && xhr.status == 200) 
-			{
-				switch(xhr.responseText)
+	if(!empty(pseudo))
+	{
+		if(verifAlphaNum(pseudo) == true)
+		{	
+			var xhr = new XMLHttpRequest();
+			xhr.open('GET', 'http://localhost/Bob-Project/index.php?ajax=existe_membre&pseudo=' + pseudo);
+			xhr.send(null);
+			xhr.onreadystatechange = function() {
+				if(xhr.readyState == 4 && xhr.status == 200) 
 				{
-					case "0" : document.getElementById("pseudoEtat").innerHTML = "Pseudo ok";
-					break;
-					case "1" : document.getElementById("pseudoEtat").innerHTML = "Pseudo pas ok";
-					break;
-					case "Erreur" : default : document.getElementById("pseudoEtat").innerHTML = "Erreur ajax contacter administrateur";
-				};
-			}		
-		};
+					switch(xhr.responseText)
+					{
+						case "0" : 
+							document.getElementById("pseudoEtat").innerHTML = "Pseudo ok";
+						break;
+						
+						case "1" : 
+							document.getElementById("pseudoEtat").innerHTML = "Pseudo pas ok";
+						break;
+						
+						case "Erreur" : 
+						default : 
+							document.getElementById("pseudoEtat").innerHTML = "Erreur ajax contacter administrateur";
+						break;
+					};
+				}		
+			};
+		}
+		else
+		{
+			document.getElementById("pseudoEtat").innerHTML = "Pseudo incorrect (syntaxe)";
+		}	
 	}
 	else
 	{
-		document.getElementById("pseudoEtat").innerHTML = "Pseudo incorrect (syntaxe)";
-	}	
+		document.getElementById("pseudoEtat").innerHTML = "Pseudo non renseigné";
+	}
+	
 
 };	
 	
 
 /* http://localhost/Bob-Project/index.php?ajax=existe_membre&pseudo=root */
+/* document.getElementById("pseudoEtat").innerHTML */
+/* document.getElementById("idImage").src = "unLien/vers/uneImage.bmp"; */
