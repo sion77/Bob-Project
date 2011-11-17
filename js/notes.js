1c2be72bee86dd5f0e63de1b35fd2975134ed475
Thomas Levasseur <levasseur.tom@gmail.com>
2011-11-17 18:18:28 +0100

    Ajout mouseover etoiles

--- js/notes.js
+++ js/notes.js
@@ -10,3 +10,23 @@ function etoile (e)
 	document.getElementById("note").value = e;
 
 }
+
+function focusetoile (t)
+{
+	var i;
+	for(i = 1; i <= t; i++)
+		document.getElementById("star"+i).src = "img/yellowstar.png";	
+	for(i = t+1; i <= 5; i++)
+		document.getElementById("star"+i).src = "img/greystar.png";
+	
+}
+
+function loosefocus ()
+{
+	var i;	
+	for(i = 1; i <= document.getElementById("note").value; i++)
+		document.getElementById("star"+i).src = "img/yellowstar.png";
+	for(i = 5 ; i > document.getElementById("note").value; i--)
+		document.getElementById("star"+i).src = "img/greystar.png";
+	
+}
