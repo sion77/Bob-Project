7f356b661fc4db655078c403c02736a211a560bc
Sylafrs <sylaf@hotmail.fr>
2011-11-25 17:21:17 +0100

    Correction d'un bug dans le rapport

--- rapport/Code/latex/clean.sh
+++ rapport/Code/latex/clean.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-echo "clean.sh >> rm -r *.log rapport.toc rapport.aux rapport.log rapport.synctex.gz"
-rm -r *.log rapport.toc rapport.aux rapport.log rapport.synctex.gz
+echo "clean.sh >> rm -rf *.log rapport.toc rapport.aux rapport.log rapport.synctex.gz"
+rm -rf *.log rapport.toc rapport.aux rapport.log rapport.synctex.gz
 
-echo "clean.sh >> *~ ../rapport.pdf"
-rm -r *~ ../rapport.pdf
+echo "clean.sh >> rm -rf *~ ../rapport.pdf"
+rm -rf *~ ../rapport.pdf
 
 echo "clean.sh >> mv rapport.pdf ../rapport.pdf"
 mv rapport.pdf ../rapport.pdf
